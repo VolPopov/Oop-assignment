2ca4a0f7e5e6af6e690acf36dbb45566eb6898f9
VolPopov <volodjapopov01@gmail.com>
2025-03-11 11:40:19 +0100

    cleaned up some code in main assignment

--- main.js
+++ main.js
@@ -7,35 +7,31 @@ PATIENT_INFORMATION["PHONE_NUMBER"], PATIENT_INFORMATION["DISEASE_HISTORY"], PAT
 
 let patientList = [];
 
-patient.prikaziPodatke();
-
-console.log();
-
-
 let doctor = new Lekar(DOCTOR_INFORMATION["NAME"], DOCTOR_INFORMATION["SURNAME"], DOCTOR_INFORMATION["DATE_OF_BIRTH"], 
     DOCTOR_INFORMATION["PHONE_NUMBER"], DOCTOR_INFORMATION["SPECIALIZATION"], patientList);
-
-doctor.prikaziPodatke();
 
 let listOfDoctors = [];
 let listOfPatients = [];
 let odeljenje = new BolnickoOdeljenje("Kardiologija", listOfPatients, listOfDoctors);
 
 odeljenje.dodaj_lekara(doctor);
-odeljenje.prikaziPodatkeOdeljenja();
 
 let novaUsluga = new Usluga(SERVICE_INFORMATION["NAME"], SERVICE_INFORMATION["PRICE"], SERVICE_INFORMATION["DATE"]);
-novaUsluga.prikaziPodatkeUsluge();
 
 odeljenje.prijemPacijenta(patient);
-odeljenje.prikaziPodatkeOdeljenja();
 
 patient.zakaziPregled(doctor, novaUsluga);
 
-doctor.prikaziPodatke();
-console.log(doctor.lista_pacijenata);
-
-console.log(patient.trenutno_stanje);
-
 doctor.postaviDijagnozu(patient, "Blaga aritmija");
+
+patient.prikaziPodatke();
+console.log();
+doctor.prikaziPodatke();
+console.log();
+odeljenje.prikaziPodatkeOdeljenja();
+console.log();
+novaUsluga.prikaziPodatkeUsluge();
+console.log();
+console.log(doctor.lista_pacijenata);
+console.log();
 console.log(patient.trenutno_stanje);
